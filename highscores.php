68ecc914f1598045f166896da78b16e2d9c62b26
Mforrest20 <mforrest10@gmail.com@users.noreply.github.com>
2021-03-15 15:00:10 -0400

    Moved page files and all folders to root. Changed related file associations. Removed test folder.

---
+++ highscores.php
@@ -0,0 +1,89 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!-- PHP code starts with forced HTTPS check -->
+<?php
+//Load credentials config file from remote folder
+$configfile = require '../config/config.php';
+$config= $configfile['hsdb'];
+
+$dsn = "mysql:host=".$config['host'].";dbname=".$config['db'].";charset=".$config['charset'];
+
+
+try {
+     $pdo = new PDO($dsn, $config['user'], $config['pass'], $config['options']);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+//Fetch all 10 possible entries
+$scores = $pdo->query('SELECT * from highscores ORDER BY hs_score DESC LIMIT 10')->fetchAll();
+if($scores){
+	echo "I ran";
+	//iterate through each entry
+	foreach ($scores as $row)
+	{
+		$namesTemp .= "<li>".$row['hs_initials']."</li>";
+		$scoresTemp .= "<li>".$row['hs_score']."</li>";
+	}
+}
+
+//If no entries found
+else{
+	$namesTemp = "0 results";
+	$scoreTemp = "0 results";
+}
+//Close the connection
+$conn = null;
+?>
+
+
+
+
+
+
+<head>
+	<title>Phaser Drifter | About Us</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mansalva">
+	<link rel="stylesheet" type="text/css" href="styles/mainpage.css" title="style" />
+</head>
+
+
+<body>
+
+<div class="logoNavbar">
+	<div class="logo">
+	<img src="assets/final_logo.jpg" alt="Phaser Drifter Logo" height="125">
+	</div>
+	
+	<div class="navContain">
+		<div><a href="index.html"><button class="button w3-mansalva">Game</button></a></div>
+		<div><a href="highscores.php"><button class="button w3-mansalva">Highscores</button></a></div>
+		<div><a href="https://www.youtube.com/watch?v=V5hyo21Zl9k"><button class="button w3-mansalva">Youtube</button></a></div>
+	</div>
+</div>
+
+<div class="w3-mansalva logoNavbar">
+	<h1>High Scores</h1>
+</div>
+
+<div class="w3-mansalva highScoreContainer">
+	<div class="name">
+		<h3>Name:</h3>
+		<ol style="margin-left: 14px;">
+		<?php   if (isset($namesTemp)) echo $namesTemp; ?>
+		</ol>
+	</div>
+	<div class="score">
+		<h3>Score:</h3>
+		<ul style="list-style-type:none;">
+		<?php   if (isset($scoresTemp)) echo $scoresTemp; ?>
+		</ul>
+	</div>
+</div>
+</body>
+
+</html>
---
+++ update.php
@@ -0,0 +1,53 @@
+<?php
+
+//Load Cookies and Set variables from cookie
+if($_COOKIE) {
+	foreach ($_COOKIE as $key=>$val)
+	{
+		if ($key =='newName'){
+			$newName = $val;
+		}
+		else if ($key =='newScore'){
+			$newScore = $val;
+		}
+		else if ($key =='showScore'){
+			$showScorePage = $val;
+		}
+
+	}
+}
+else
+{
+	echo "No Cookies are Set";    
+}
+
+$newDate = date('Y-m-d H:i:s');
+
+//echo($newName."<br>".$newScore."<br>".$showScorePage);
+
+//Load credentials config file from remote folder
+$configfile = require '../config/config.php';
+$config= $configfile['hsdb'];
+
+$dsn = "mysql:host=".$config['host'].";dbname=".$config['db'].";charset=".$config['charset'];
+echo $dsn."<br>";
+
+try {
+     $pdo = new PDO($dsn, $config['user'], $config['pass'], $config['options']);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$sql = "INSERT INTO highscores (hs_score,hs_initials,hs_date) VALUES (?,?,?)"; 
+$pdo->prepare($sql)->execute([$newScore,$newName,$newDate]);
+
+//Close the connection
+$conn = null;
+
+if($showScorePage=="yes"){
+	header("Location: highscores.php");
+}
+else{
+	header("Location: index.html");
+}
+?>
